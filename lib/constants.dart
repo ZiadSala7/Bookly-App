7366bfd014d12877afae5afd40078d97cd9a0c19
Ziad Salah <zslah2800@gmail.com>
2024-06-25 14:39:31 +0300

    splash view ui and animation

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xff100B20);
+const Duration kTransitionDuration = Duration(seconds: 2);
+const String kGtSectraFine = "GT Sectra Fine";
+const double homeViewPadding = 30;
+const String testImgUrl =
+    'https://images.search.yahoo.com/search/images;_ylt=AwrhdgRPpwxmH5QRWbiJzbkF;_ylu=c2xrA3RleHQEaXQDQWxzb1RyeV9OBHNlYwNyZWwEcG9zAzE-?p=Free+Stock+Photography+Websites&fr2=p%3As%2Cv%3Ai%2Cm%3Ars-top%2Cct%3Abing%2Crgn%3Atop%2Cpos%3A1&fr=mcafee&type=E210US91105G0';
