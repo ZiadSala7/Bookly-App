accb7b157e6cadee2ec96aaf3ed2a56f7bae4866
Ziad Salah <zslah2800@gmail.com>
2024-07-08 11:46:45 +0300

    book model edits

--- lib/features/home/data/models/book_model/retail_price.dart
+++ lib/features/home/data/models/book_model/retail_price.dart
@@ -1,21 +1,16 @@
-import 'package:equatable/equatable.dart';
+class RetailPrice {
+	double? amount;
+	String? currencyCode;
 
-class RetailPrice extends Equatable {
-  final double? amount;
-  final String? currencyCode;
+	RetailPrice({this.amount, this.currencyCode});
 
-  const RetailPrice({this.amount, this.currencyCode});
+	factory RetailPrice.fromJson(Map<String, dynamic> json) => RetailPrice(
+				amount: (json['amount'] as num?)?.toDouble(),
+				currencyCode: json['currencyCode'] as String?,
+			);
 
-  factory RetailPrice.fromJson(Map<String, dynamic> json) => RetailPrice(
-        amount: (json['amount'] as num?)?.toDouble(),
-        currencyCode: json['currencyCode'] as String?,
-      );
-
-  Map<String, dynamic> toJson() => {
-        'amount': amount,
-        'currencyCode': currencyCode,
-      };
-
-  @override
-  List<Object?> get props => [amount, currencyCode];
+	Map<String, dynamic> toJson() => {
+				'amount': amount,
+				'currencyCode': currencyCode,
+			};
 }
